18169114db65de7f55ba590c7bea5a0e0935eef5
chongmegan <chong.m@northeastern.edu>
2020-10-27 19:26:31 -0400

    added product specs, but still trying to fix img 'fluid' error

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,14 @@ import Hero from '../components/hero'
 import Layout from '../components/layout'
 import ArticlePreview from '../components/article-preview'
 import HeroImage from '../components/hero.js'
+import ProductSpecs from '../components/product-specs.js'
+import base from '../components/base.css'
 
 class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title');
     const heroImage = get(this, 'props.data.contentfulHeroImage');
+    const productSpecs = get(this, 'props.data.contentfulProductSpecs');
     
     return (
       <Layout location={this.props.location}>
@@ -18,6 +21,8 @@ class RootIndex extends React.Component {
           <Helmet title={siteTitle} />
           <HeroImage data= {heroImage} />
         </div>
+        <h1 className="productSpecs">Product Specs</h1>
+        <ProductSpecs data={productSpecs}/>
       </Layout>
     )
   }
@@ -49,6 +54,19 @@ export const pageQuery = graphql`
         fluid {
           src
         }
+      }
+    }
+    contentfulProductSpecs(contentful_id: {eq: "31xhfbVRrqekDhzY4hx6fd"}) {
+      id
+      title
+      productPreviewImage {
+        fluid(maxWidth: 250) {
+          src
+        }
+        id
+      }
+      specDescription {
+        specDescription
       }
     }
   }
