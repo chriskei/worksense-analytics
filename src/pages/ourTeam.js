e0a4c6d708f20250720d465aa60dd97f5b4c103f
chongmegan <chong.m@northeastern.edu>
2020-10-29 18:20:08 -0400

    Added brackets to layout

--- src/pages/ourTeam.js
+++ src/pages/ourTeam.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
-import Layout from '../components/layout'
+import { Layout } from '../components/layout'
 
-const ourTeamPage = (props) => {
+const ourTeamPage = ( props ) => {
     return (
         <Layout>
             <h1>Our Team Page Placeholder</h1>
