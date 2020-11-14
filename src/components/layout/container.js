b0fc47e787d1f7727558fbf97212bf9832ef2542
mnav0 <mnavracruz@gmail.com>
2020-11-13 19:13:37 -0500

    Fixed imports

--- src/components/layout/container.js
+++ src/components/layout/container.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { colors } from '../assets/colors.js'
+import { colors } from '../../assets/colors.js'
 
 export default ({ children }) => (
   <div style={{ margin: '0 0', backgroundColor: `${colors.tan}` }}>
