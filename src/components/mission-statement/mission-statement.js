a26719b5b5dd3539fef7da343532be7285ad0037
mnav0 <mnavracruz@gmail.com>
2020-11-30 10:22:28 -0500

    Fixed code styling

--- src/components/mission-statement/mission-statement.js
+++ src/components/mission-statement/mission-statement.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import { MissionContainer, MissionHeader, MissionText } from './mission-statement.styles'
+import {
+  MissionContainer,
+  MissionHeader,
+  MissionText
+} from './mission-statement.styles'
 import { Mission } from '../../assets/headers'
 
 const MissionStatement = (props) => {
